33f395790b3f4aa40e41e56ea3545c65693b133f
asavershin <avershinrussian@gmail.com>
2024-10-12 17:31:49 +0300

    6 Fix checkstyle error

--- src/main/java/agro/filelinkhub/domain/upload/tiff/MultiLayerTiffPresignedUrlGenerator.java
+++ src/main/java/agro/filelinkhub/domain/upload/tiff/MultiLayerTiffPresignedUrlGenerator.java
@@ -3,9 +3,9 @@ package agro.filelinkhub.domain.upload.tiff;
 import static io.minio.http.Method.PUT;
 
 import agro.filelinkhub.configs.annotations.Query;
+import agro.filelinkhub.domain.exceptions.LinksGenerationException;
 import agro.filelinkhub.domain.upload.Link;
 import agro.filelinkhub.domain.upload.PresignedUrlGenerator;
-import agro.filelinkhub.domain.exceptions.LinksGenerationException;
 import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
 import java.util.List;
