ef7da1f6fc129cc12785c23720a248f7cb2bd3fd
asavershin <avershinrussian@gmail.com>
2024-10-24 15:11:14 +0300

    10 Rm useless s3 methods

--- src/main/java/agro/filelinkhub/domain/S3Repo.java
+++ src/main/java/agro/filelinkhub/domain/S3Repo.java
@@ -1,15 +1,9 @@
 package agro.filelinkhub.domain;
 
-import java.io.ByteArrayInputStream;
-
 public interface S3Repo {
-
-  void upload(ByteArrayInputStream inputStream, String bucketName, String objectName, int size);
 
   String uploadUrl(String name, String bucket, int expiration);
 
   String loadUrl(String name, String bucket, int expiration);
-
-  void save(Object object);
 
 }
